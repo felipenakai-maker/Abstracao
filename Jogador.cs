a29e331a01618870bea278b6e84e996f7777ea68
felipenakai-maker <felipenakai@edu.senai.br>
2026-08-28 09:52:03 -0300

    Exer 4

---
+++ Jogador.cs
@@ -0,0 +1,16 @@
+﻿namespace Abstracao
+{
+    internal class Jogador
+    {
+        public string Nome;
+        public string Posicao;
+        public int NumCamisa;
+
+        public Jogador(string nome, string posicao, int numeroCamisa)
+        {
+            Nome = nome;
+            Posicao = posicao;
+            NumCamisa = numeroCamisa;
+        }
+    }
+}
---
+++ Time.cs
@@ -0,0 +1,28 @@
+﻿namespace Abstracao
+{
+    internal class Time
+    {
+        public string NomeDoTime;
+        public List<Jogador> ListaDeJogadores;
+        public Time(string nomeDoTime)
+        {
+            NomeDoTime = nomeDoTime;
+            ListaDeJogadores = new List<Jogador>();
+        }
+
+        public void AdicionarJogador(Jogador jogador)
+        {
+            ListaDeJogadores.Add(jogador);
+        }
+
+        public void ListarJogadores()
+        {
+            Console.WriteLine();
+            Console.WriteLine($"Escalação do {NomeDoTime}:");
+            foreach (Jogador jogador in ListaDeJogadores)
+            {
+                Console.WriteLine($"- {jogador.Nome} | Posição: {jogador.Posicao} | Camisa: {jogador.NumCamisa}");
+            }
+        }
+    }
+}
